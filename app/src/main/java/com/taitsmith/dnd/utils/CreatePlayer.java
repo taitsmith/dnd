620b2bc3ef2ecd47f378279ccd26fd736d93fe27
tait smith <taitsmith@protonmail.com>
2020-07-07 15:29:45 -0700

    add random player creator

--- app/src/main/java/com/taitsmith/dnd/utils/CreatePlayer.java
+++ app/src/main/java/com/taitsmith/dnd/utils/CreatePlayer.java
@@ -1,10 +1,44 @@
 package com.taitsmith.dnd.utils;
 
+
+import android.content.Context;
+
 import com.taitsmith.dnd.objects.Player;
+
+import io.realm.Realm;
 
 public class CreatePlayer {
 
-    public Player createCharacter(Boolean isRandom) {
-        return new Player();
+    //TODO remove this, it's just random character for testing
+    public static void randomPlayer(Context context) {
+        Realm.init(context);
+        Realm realm = Realm.getDefaultInstance();
+
+        realm.beginTransaction();
+        Player player = realm.createObject(Player.class);
+
+        player.setLevel(10);
+        player.setName("Greg");
+        player.setRace("Elf");
+        player.setClazz("Cleric");
+        player.setBackground("Criminal");
+        player.setAlignment("Chaotic Evil");
+
+        player.setIntel(15);
+        player.setDex(20);
+        player.setStr(3);
+        player.setCon(18);
+        player.setWis(13);
+        player.setCha(10);
+
+        player.setXp(5000);
+
+        player.setHpMax(48);
+        player.setHpCurrent(45);
+        player.setHpTemp(0);
+
+        player.setHitDice(6);
+
+        realm.commitTransaction();
     }
 }
